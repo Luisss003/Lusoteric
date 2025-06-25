13f53d5824d3cd23150491e937a937918b3ac288
Luisss003 <luis.saenz.compsci@gmail.com>
2025-06-24 23:30:56 -0500

    Created challenge model.

---
+++ models/challengeModel.js
@@ -0,0 +1,36 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('./database');
+
+const Challenge = sequelize.define(
+    'Challenge',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        challenge: {
+            type: DataTypes.TEXT,
+            allowNull:false,
+        },
+        language:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        category: {
+            type:DataTypes.ENUM('Code Golf', 'Obfuscation', 'Esolang', 'One-Liner', 'Forbidden Keywords', 'Poetic Code')
+        },
+        user_id:{
+            type:DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+    },
+    {
+        sequelize,
+        modelName: 'Challenge',
+    },
+);
+
+
+module.exports = Challenge;
