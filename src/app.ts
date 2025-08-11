8d103d2098c97e1af480e4adc94a6c8c6c513a07
Luisss003 <lsaenz29484@gmail.com>
2025-08-10 22:33:33 -0500

    Added challenge, code and default handler route.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
-import challengesRouter from './routes/challengesRoutes';
-
+import challengesRouter from './routes/challengesRoutes.js';
+import codeRouter from './routes/codeRoutes.js';
 let app = express();
 
 app.use(express.json());
 
-app.use('/api/v1/challenges', challengesRouter);
+//Routes
+app.use('/challenges', challengesRouter);
+app.use('/code', codeRouter);
+
+// Error handling middleware
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.status(404).json({ message: 'Not Found' });
+});
+
 
 export default app;
