ad2dc6c9a51ae20ccf9972164ff45b90e83b5b0a
Luisss003 <lsaenz29484@gmail.com>
2025-08-10 22:34:03 -0500

    Fixed imports.

--- src/routes/codeRoutes.ts
+++ src/routes/codeRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as codeController from './../controllers/codeController';
+import * as codeController from './../controllers/codeController.js';
 const router = express.Router();
 
 /**
@@ -8,12 +8,14 @@ const router = express.Router();
  *      POST: Create a code submission
  */
 router.route('/')
-    .get(CodeController.getCodeSubmissions)
-    .post(CodeController.createCodeSubmission);
+    .get(codeController.getCodeSubmissions)
+    .post(codeController.createCodeSubmission);
 
 /**
  * :8000/code/:id
  *      GET: Get code submission by ID
  */
 router.route('/:id')
-    .get(CodeController.getCodeSubmissionById);
+    .get(codeController.getCodeSubmissionById);
+
+export default router;
