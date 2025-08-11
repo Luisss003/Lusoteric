7bc5e9e529d5563f6510b8f83f7d448c6c051cff
Luisss003 <lsaenz29484@gmail.com>
2025-08-10 22:33:51 -0500

    Fixed imports and used centralized sequelize instance.

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,19 @@
-import app from './app';
 import dotenv from 'dotenv';
-import { Sequelize } from 'sequelize';
-
-const Challenge = require('./models/challengeModel');
-const Code = require('./models/codeModel');
-const User = require('./models/userModel');
-
-// Load env variables
 dotenv.config({ path: './config.env' });
 
-//Create Sequelize instance
-export const sequelize = new Sequelize('Lusoteric', process.env.MY_SQL_USERNAME!, 
-                                process.env.MY_SQL_PASSWRD!, {
-                                    host: process.env.MY_SQL_HOST!,
-                                    dialect: 'mysql',
-                                });
+import app from './app.js';
+import Challenge from './models/challengeModel.js';
+import Code from './models/codeModel.js';
+import User from './models/userModel.js';
+import sequelize from './utils/sequelize.js';
 
+// Sync models with the database
 (async () => {
     try {
+        // Test DB connection
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
+        // Sync models
         const ChallengeSynced = await Challenge.sync();
         const CodeSynced = await Code.sync();
         const UserSynced = await User.sync();
