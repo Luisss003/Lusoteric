6d13d2746423049fd39356f4ff2ffbe32e6e5812
Luisss003 <145069480+Luisss003@users.noreply.github.com>
2025-08-10 22:41:31 -0500

    Added req,res,next TS Types for middlewares + additional comments.

--- src/controllers/codeController.ts
+++ src/controllers/codeController.ts
@@ -1,10 +1,14 @@
 import asyncErrorHandler from "../utils/asyncErrorHandler";
 import Code from './../models/codeModel';
+import { Request, Response, NextFunction } from 'express';
 
 //GET - /api/v1/code
-export const getCodeSubmissions = asyncErrorHandler(async(req, res, next) => {
+export const getCodeSubmissions = asyncErrorHandler(
+    async(req: Request, res: Response, next: NextFunction) => {
+    //Fetch all code submissions from the database
     const codeSubmissions = await Code.findAll();
 
+    //Return 200 status code and the list of code submissions
     res.status(200).json({
         status: "success",
         length: codeSubmissions.length,
@@ -15,9 +19,13 @@ export const getCodeSubmissions = asyncErrorHandler(async(req, res, next) => {
 });
 
 //POST - /api/v1/code
-export const createCodeSubmission = asyncErrorHandler(async(req, res, next) => {
+export const createCodeSubmission = asyncErrorHandler(
+    async(req: Request, res: Response, next: NextFunction) => {
+    
+    //Create new code submission based on request body
     const newCodeSubmission = await Code.create(req.body);
 
+    //Return 201 status code and the new code submission if successful
     res.status(200).json({
         status: "status",
         data: {
@@ -26,13 +34,18 @@ export const createCodeSubmission = asyncErrorHandler(async(req, res, next) => {
     });
 });
 
-export const getCodeSubmissionById = asyncErrorHandler(async(req, res, next) => {
+//GET - /api/v1/code/:id
+export const getCodeSubmissionById = asyncErrorHandler(
+    async(req: Request, res: Response, next: NextFunction) => {
+    
+    //Find code submission by ID
     const code = await Code.findAll({
         where: {
             id: req.params.id
         }
     });
 
+    //Return 404 if not found
     if(!code){
         return res.status(404).json({
             status: "failed",
@@ -40,6 +53,7 @@ export const getCodeSubmissionById = asyncErrorHandler(async(req, res, next) => 
         });       
     }
 
+    //Return code submission if found
     res.status(200).json({
         status: "success",
         data: {
