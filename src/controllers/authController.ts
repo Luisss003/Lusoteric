66a565a13590fcba98a7cc50f6f612ed6a23b26c
Luisss003 <145069480+Luisss003@users.noreply.github.com>
2025-08-11 21:59:05 -0500

    Send JWT with Signup

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import asyncErrorHandler from '../utils/asyncErrorHandler.js';
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 export const signup = asyncErrorHandler(
     async (req: Request, res: Response) => {
@@ -9,8 +10,12 @@ export const signup = asyncErrorHandler(
         //Assuming req has been validated and sanitized
         const newUser = await User.create(req.body);
 
+        //Create and send JWT token for the new user
+        const token = jwt.sign({id: newUser.id}, process.env.JWT_SECRET_KEY!, {})
+
         res.status(201).json({
             status: "success",
+            token: token,
             data: {
                 user: newUser
             }
