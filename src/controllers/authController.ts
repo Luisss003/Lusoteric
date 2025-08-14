818225fa0394ef7a6dd0c4e7921fe678a10b79e2
Luisss003 <145069480+Luisss003@users.noreply.github.com>
2025-08-13 21:52:35 -0500

    Implemented log-in logic for auth controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,6 +2,7 @@ import User from '../models/userModel.js';
 import asyncErrorHandler from '../utils/asyncErrorHandler.js';
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt, { compare } from 'bcrypt';
 
 export const signup = asyncErrorHandler(
     async (req: Request, res: Response) => {
@@ -23,3 +24,51 @@ export const signup = asyncErrorHandler(
 
     });
 
+export const login = asyncErrorHandler(
+    async (req: Request, res: Response) => {
+        //Extract email and password from request body
+        const {username, password} = req.body;
+
+        if(!username || !password) {
+            return res.status(400).json({
+                status: "fail",
+                message: "Please provide email and password"
+            });
+        };
+
+        //Find user by email, and exclude password from the response
+        const user = await User.findOne({
+            where: {
+                username: username,
+            },
+        });
+
+        if(!user) {
+            return res.status(401).json({
+                status: "fail",
+                message: "Incorrect username or password"
+            });
+        };
+
+        //Check if password matches
+        const isMatch = await user.comparePassword(password);
+
+        if(!isMatch) {
+            return res.status(401).json({
+                status: "fail",
+                message: "Incorrect username or password"
+            });
+        };
+
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET_KEY!, {})
+
+
+        res.status(200).json({
+            status: "success",
+            token: token,
+            data: {
+                user: user
+            }
+        });
+
+    });
