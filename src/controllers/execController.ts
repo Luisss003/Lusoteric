51edf3491a6213838b449125e9cacacd6c52b020
Luisss003 <145069480+Luisss003@users.noreply.github.com>
2025-09-29 22:40:54 -0500

    Added testcase functionality for Java, javacsript, C, Go, Rust, Bash, x86, and working on Chef and Holy C

--- src/controllers/execController.ts
+++ src/controllers/execController.ts
@@ -11,11 +11,11 @@ export const executeSubmission = asyncErrorHandler(
         const {testcases} = (req as any);
         console.log(testcases)
 
-        let output = '';
-        let o: {
-            output: string
-            passed: boolean
-        }
+        
+        let output: { output: string; passed: boolean } = {
+            output: "",
+            passed: false
+        };
         
         if(!challengeId || !code){
             return res.status(404).json({
@@ -37,37 +37,42 @@ export const executeSubmission = asyncErrorHandler(
         //Execute util function based on language
         switch(language){
             case "BASH":
-                output = await execUtils.executeBash(code);
+                output = await execUtils.executeBash(code, testcases);
                 break;
             case "C":
                 output = await execUtils.executeC(code, testcases);
                 break;
             case "CHEF":
-                output = await execUtils.executeChef(code);
+                //output = await execUtils.executeChef(code, testcases);
                 break;
             case "GO":
-                output = await execUtils.executeGo(code);
+                output = await execUtils.executeGo(code, testcases);
                 break;
             case "HOLY_C":
-                output = await execUtils.executeHolyC(code);
+                //output = await execUtils.executeHolyC(code);
                 break;
             case "JAVA":
-                output = await execUtils.executeJava(code);
+                output = await execUtils.executeJava(code, testcases);
                 break;
             case "JAVASCRIPT":
-                output = await execUtils.executeJavaScript(code);
+                output = await execUtils.executeJavaScript(code, testcases);
                 break;
             case "PYTHON":
                 break;
             case "RUST":
-                output = await execUtils.executeRust(code);
+                output = await execUtils.executeRust(code, testcases);
                 break;
             case "SHAKESPEARE":
-                output = await execUtils.executeShakespeare(code);
+                output = await execUtils.executeShakespeare(code, testcases);
                 break;
             case "X_86":
-                output = await execUtils.executeX86(code);
+                output = await execUtils.executeX86(code, testcases``);
                 break;
+            default:
+                return res.status(404).json({
+                    status: "failure",
+                    message: "Language not supported..."
+                })
         }
     
         res.status(200).json({
--- src/utils/executeUtils.ts
+++ src/utils/executeUtils.ts
@@ -49,117 +49,202 @@ export async function executeC(
     };
 }
 
-export async function executeJava(code: string) {
-    const tempFilePath = '/tmp/Main.java'; // must match `public class Main`
-    await writeFile(tempFilePath, code, 'utf-8');
+export async function executeJava(
+    code: string,
+    testCases: { input: string; output: string }[]
+    ): Promise<{ output: string; passed: boolean }> {
+    
+        const tempFilePath = '/tmp/Main.java'; 
+        await writeFile(tempFilePath, code, 'utf-8');
+        let allPassed = true;
+        let results: string[] = [];
 
-    const { stdout, stderr } = await execAsync(`
-        docker run --rm \
-        --memory=512m \
-        --cpus=1 \
-        --network=none \
-        --read-only \
-        --tmpfs /tmp:exec,size=50m \
-        -v ${tempFilePath}:/usr/src/app/Main.java:ro \
-        -w /usr/src/app \
-        openjdk:latest \
-        sh -c "javac -d /tmp Main.java && java -cp /tmp Main"
-    `, {
-        timeout: 15000,
-        maxBuffer: 1024 * 1024
-    });
+        for(const testCase of testCases){
+            const { stdout } = await execAsync(`
+                docker run --rm \
+                --memory=512m \
+                --cpus=1 \
+                --network=none \
+                --read-only \
+                --tmpfs /tmp:exec,size=50m \
+                -v ${tempFilePath}:/usr/src/app/Main.java:ro \
+                -w /usr/src/app \
+                openjdk:latest \
+                sh -c "javac -d /tmp Main.java && echo '${testCase.input}' | java -cp /tmp Main"
+            `, { timeout: 15000, maxBuffer: 1024 * 1024 });
 
-    await unlink(tempFilePath);
-    return stdout;
+            const got = stdout.trim();
+            const passed = got === testCase.output;
+
+            if(!passed) allPassed = false;
+            results.push(
+                `input: ${testCase.input}, got: ${got}, expected: ${testCase.output}, result: ${passed ? 'success' : 'fail'}`
+            )
+        };
+
+        await unlink(tempFilePath);
+        return  {
+            output: results.join('\n'),
+            passed: allPassed
+        } ;
 }
-export async function executeJavaScript(code: string){
+export async function executeJavaScript(
+    code: string, 
+    testCases: { input: string; output: string }[]
+): Promise<{ output: string; passed: boolean }> {
     const tempFilePath = '/tmp/main.js';
     await writeFile(tempFilePath, code, 'utf-8');
 
-    const {stdout, stderr} = await execAsync(`
-        docker run --rm \
-        --memory=512m \
-        --cpus=1 \
-        --network=none \
-        --read-only \
-        --tmpfs /tmp:exec,size=50m \
-        -v ${tempFilePath}:/usr/src/app/main.js:ro \
-        -w /usr/src/app \
-        node:latest \
-        sh -c "node main.js"
-        `,{
-        timeout: 15000,
-        maxBuffer: 1024 * 1024
-    });
+    const results: string[] = [];
+    let allPassed = true;
 
-    await unlink(tempFilePath);
-    return stdout;
-}
-export async function executeGo(code: string){
-    const tempFilePath = '/tmp/main.go';
-    await writeFile(tempFilePath, code, 'utf-8');
-
-    const {stdout, stderr} = await execAsync(`
-        docker run --rm \
-        --memory=512m \
-        --cpus=1 \
-        --network=none \
-        --tmpfs /tmp:exec,size=50m \
-        -v ${tempFilePath}:/usr/src/app/main.go:ro \
-        -w /usr/src/app \
-        golang:latest \
-        sh -c "go run main.go"
-        `,{
-        timeout: 30000,
-        maxBuffer: 1024 * 1024
-    });
-
-    await unlink(tempFilePath);
-    return stdout;
-}
-export async function executeRust(code: string){
-    const tempFilePath = '/tmp/main.rs';
-    await writeFile(tempFilePath, code, 'utf-8');
-
-    const {stdout, stderr} = await execAsync(`
-        docker run --rm \
-        --memory=512m \
-        --cpus=1 \
-        --network=none \
-        --tmpfs /tmp:exec,size=50m \
-        -v ${tempFilePath}:/usr/src/app/main.rs:ro \
-        -w /usr/src/app \
-        rust:latest \
-        sh -c "rustc main.rs -o main && ./main"
-        `,{
-        timeout: 30000,
-        maxBuffer: 1024 * 1024
-    });
-
-    await unlink(tempFilePath);
-    return stdout;
-}
-export async function executeX86(code: string){
-    const tempFilePath = '/tmp/main.asm';
-    await writeFile(tempFilePath, code, 'utf-8');
-
-    const {stdout, stderr} = await execAsync(`
-        docker run --rm \
+    for (const testCase of testCases) {
+        const { stdout } = await execAsync(
+            `docker run --rm \
             --memory=128m \
-            --cpus=.5 \
+            --cpus=0.5 \
             --network=none \
-            --tmpfs /tmp:exec,size=100m \
-            -v ${tempFilePath}:/usr/src/app/main.asm:ro \
+            --read-only \
+            --tmpfs /tmp:exec,size=50m \
+            -v ${tempFilePath}:/usr/src/app/main.js:ro \
             -w /usr/src/app \
-            esolang/x86asm-nasm \
-            bash -c "x86asm-nasm main.asm"
-        `,{
-            timeout: 10000
-        })
+            node:latest \
+            sh -c "echo '${testCase.input}' | node main.js"
+            `,{ timeout: 15000, maxBuffer: 1024 * 1024 }
+        );
+
+        const got = stdout.trim();
+        const passed = got === testCase.output;
+
+        if (!passed) allPassed = false;
+
+        results.push(
+            `input: ${testCase.input}, got: ${got}, expected: ${testCase.output}, result: ${passed ? 'success' : 'fail'}`
+        );
+    }
+
 
     await unlink(tempFilePath);
-    return stdout;
+    return {
+        output: results.join('\n'),
+        passed: allPassed
+    };
+}
 
+export async function executeGo(
+    code: string, 
+    testCases: { input: string; output: string }[]
+): Promise<{ output: string; passed: boolean }> {
+        const tempFilePath = '/tmp/main.go';
+        await writeFile(tempFilePath, code, 'utf-8');
+        let allPassed = true;
+        let results: string[] = [];
+
+        for(const testCase of testCases){
+            const { stdout } = await execAsync(`
+                docker run --rm \
+                --memory=512m \
+                --cpus=1 \
+                --network=none \
+                --tmpfs /tmp:exec,size=50m \
+                -v ${tempFilePath}:/usr/src/app/main.go:ro \
+                -w /usr/src/app \
+                golang:latest \
+                sh -c "echo '${testCase.input}' | go run main.go"
+                `,{ timeout: 30000,maxBuffer: 1024 * 1024 });
+            
+                const got = stdout.trim();
+                const passed = got === testCase.output;
+
+                if(!passed) allPassed = false;
+                results.push(
+                    `input: ${testCase.input}, got: ${got}, expected: ${testCase.output}, result: ${passed ? 'success' : 'fail'}`
+                );
+        }
+        await unlink(tempFilePath);
+        return  {
+            output: results.join('\n'),
+            passed: allPassed
+        } ;
+}
+export async function executeRust(
+    code: string, 
+    testCases: { input: string; output: string }[]
+): Promise<{ output: string; passed: boolean }> {
+        const tempFilePath = '/tmp/main.rs';
+        await writeFile(tempFilePath, code, 'utf-8');
+        const results: string[] = [];
+        let allPassed = true;
+
+        for (const testCase of testCases) {
+            const { stdout } = await execAsync(
+                `docker run --rm \
+                --memory=128m \
+                --cpus=0.5 \
+                --network=none \
+                --read-only \
+                --tmpfs /tmp:exec,size=50m \
+                -v ${tempFilePath}:/usr/src/app/main.rs:ro \
+                -w /usr/src/app \
+                rust:latest \
+                sh -c "rustc main.rs -o main && echo '${testCase.input}' | ./main"
+                `,{ timeout: 15000, maxBuffer: 1024 * 1024 }
+            );
+
+            const got = stdout.trim();
+            const passed = got === testCase.output;
+
+            if (!passed) allPassed = false;
+
+            results.push(
+                `input: ${testCase.input}, got: ${got}, expected: ${testCase.output}, result: ${passed ? 'success' : 'fail'}`
+            );
+        }
+
+        await unlink(tempFilePath);
+        return {
+            output: results.join('\n'),
+            passed: allPassed
+        };
+}
+export async function executeX86(
+    code: string, 
+    testCases: { input: string; output: string }[]
+): Promise<{ output: string; passed: boolean }> {
+        const tempFilePath = '/tmp/main.asm';
+        await writeFile(tempFilePath, code, 'utf-8');
+        const results: string[] = [];
+        let allPassed = true;
+        for (const testCase of testCases) {
+            const { stdout } = await execAsync(
+                `docker run --rm \
+                --memory=128m \
+                --cpus=0.5 \
+                --network=none \
+                --read-only \
+                --tmpfs /tmp:exec,size=50m \
+                -v ${tempFilePath}:/usr/src/app/main.rs:ro \
+                -w /usr/src/app \
+                esolang/x86asm-nasm \
+                bash -c "echo '${testCase.input}' | x86asm-nasm main.asm"
+                `,{ timeout: 15000, maxBuffer: 1024 * 1024 }
+            );
+
+            const got = stdout.trim();
+            const passed = got === testCase.output;
+
+            if (!passed) allPassed = false;
+
+            results.push(
+                `input: ${testCase.input}, got: ${got}, expected: ${testCase.output}, result: ${passed ? 'success' : 'fail'}`
+            );
+        }
+
+        await unlink(tempFilePath);
+        return {
+            output: results.join('\n'),
+            passed: allPassed
+        };
 }
 
 //WIP
@@ -185,53 +270,92 @@ export async function executeHolyC(code: string){
     return stdout;
 }
 
-export async function executeShakespeare(code: string){
-    const tempFilePath = '/tmp/main.spl';
-    await writeFile(tempFilePath, code, 'utf-8');
-    const {stdout, stderr} = await execAsync(`
-        docker run --rm \
-            --memory=512m \
-            --cpus=1 \
-            --network=none \
-            --tmpfs /tmp:exec,size=100m \
-            -v ${tempFilePath}:/usr/src/app/main.spl:ro \
-            -w /usr/src/app \
-            luisss003/shakespeare:latest \
-            bash -c "shakespeare run main.spl"
-        `,{
-            timeout: 10000
-        });
-    
-    await unlink(tempFilePath);
-    return stdout;
+export async function executeShakespeare(
+    code: string, 
+    testCases: { input: string; output: string }[]
+): Promise<{ output: string; passed: boolean }> {
+        const tempFilePath = '/tmp/main.spl';
+        await writeFile(tempFilePath, code, 'utf-8');
+        const results: string[] = [];
+        let allPassed = true;
 
-}
-export async function executeBash(code: string){
-    const tempFilePath = '/tmp/main.sh';
-    await writeFile(tempFilePath, code, 'utf-8');
-    
-    const {stdout, stderr} = await execAsync(`
-        docker run --rm \
-            --memory=512m \
-            --cpus=1 \
-            --network=none \
-            --tmpfs /tmp:exec,size=100m \
-            -v ${tempFilePath}:/usr/src/app/main.sh:ro \
-            -w /usr/src/app \
-            luisss003/shakespeare:latest \
-            bash -c "bash main.sh"
-        `,{
-            timeout: 10000
-        });
-    
-    await unlink(tempFilePath);
-    return stdout;
-}
-export async function executeChef(code: string){
-    const tempFilePath = '/tmp/main.chef';
-    const actualCode = code.replace(/\\n/g, '\n');
-    await writeFile(tempFilePath, actualCode, 'utf-8');
-    
+        for(const testCase of testCases){
+            const { stdout } = await execAsync(`
+                docker run --rm \
+                --memory=512m \
+                --cpus=1 \
+                --network=none \
+                --tmpfs /tmp:exec,size=50m \
+                -v ${tempFilePath}:/usr/src/app/main.spl:ro \
+                -w /usr/src/app \
+                luisss003/shakespeare:latest \
+                sh -c "echo '${testCase.input}' | shakespeare run main.spl"
+                `,{ timeout: 30000,maxBuffer: 1024 * 1024 });
+            
+                const got = stdout.trim();
+                const passed = got === testCase.output;
+
+                if(!passed) allPassed = false;
+                results.push(
+                    `input: ${testCase.input}, got: ${got}, expected: ${testCase.output}, result: ${passed ? 'success' : 'fail'}`
+                );
+        }
+        await unlink(tempFilePath);
+        return  {
+            output: results.join('\n'),
+            passed: allPassed
+        } ;
+    }
+
+
+export async function executeBash(
+    code: string, 
+    testCases: { input: string; output: string }[]
+): Promise<{ output: string; passed: boolean }> {
+        const tempFilePath = '/tmp/main.sh';
+        await writeFile(tempFilePath, code, 'utf-8');
+        const results: string[] = [];
+        let allPassed = true;
+
+        for(const testCase of testCases){
+            const { stdout } = await execAsync(`
+                docker run --rm \
+                --memory=512m \
+                --cpus=1 \
+                --network=none \
+                --tmpfs /tmp:exec,size=50m \
+                -v ${tempFilePath}:/usr/src/app/main.sh:ro \
+                -w /usr/src/app \
+                bash -c "echo ${testCase.input} | bash main.sh"
+                `,{ timeout: 30000,maxBuffer: 1024 * 1024 });
+            
+                const got = stdout.trim();
+                const passed = got === testCase.output;
+
+                if(!passed) allPassed = false;
+                results.push(
+                    `input: ${testCase.input}, got: ${got}, expected: ${testCase.output}, result: ${passed ? 'success' : 'fail'}`
+                );
+        }
+        await unlink(tempFilePath);
+            return {
+                output: results.join('\n'),
+                passed: allPassed
+            };
+        }
+
+//WIP
+//NEED TO FIND BETTER CHEF INTERPRETER
+//Currently best interpreter is java, so will be creating docker image forit
+export async function executeChef(
+    code: string, 
+    testCases: { input: string; output: string }[]
+): Promise<{ output: string; passed: boolean }> {
+        const tempFilePath = '/tmp/main.chef';
+        const actualCode = code.replace(/\\n/g, '\n');
+        await writeFile(tempFilePath, actualCode, 'utf-8');
+        const results: string[] = [];
+        let allPassed = true;
     const {stdout, stderr} = await execAsync(`
         docker run --rm \
             --memory=512m \
@@ -247,5 +371,7 @@ export async function executeChef(code: string){
         });
     
     await unlink(tempFilePath);
-    return stdout;
-}
+        return {
+            output: results.join('\n'),
+            passed: allPassed
+        };}
